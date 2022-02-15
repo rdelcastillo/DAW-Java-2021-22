b37ddc5ec82bb05ac4452f17810eabe905c9fc03
Rafael del Castillo Gomariz <rafa.castillo@gmail.com>
2022-02-15 10:33:25 +0100

    Test con Junit de la clase Time

--- org/iesgrancapitan/PROGR/ejemplos/poo/time/TimeTest.java
+++ org/iesgrancapitan/PROGR/ejemplos/poo/time/TimeTest.java
@@ -1,36 +1,67 @@
 package org.iesgrancapitan.PROGR.ejemplos.poo.time;
 
-import java.util.Scanner;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
-public class TimeTest {
-  
-  private static Scanner in = new Scanner(System.in);
+class TimeTest {
 
-  public static void main(String[] args) {
-    Time time = new Time();
-    System.out.println("Hemos creado un objeto Time() vacío: " + time);
-    
-    System.out.println("Ahora vamos a crearlo desde su constructor con parámetros.");
-    time = readTime();
-    System.out.println("Objeto creado: " + time);
-    
-    System.out.print("Añade segundos (+ ó -): ");
-    
-    System.out.print("Añade minutos (+ ó -): ");
-    
-    System.out.print("Añade horas (+ ó -): ");
-  
+  @BeforeAll
+  static void setUpBeforeClass() throws Exception {}
+
+  @Test
+  void testTimeIntIntInt() {
+    fail("Not yet implemented");
   }
 
-  private static Time readTime() {
-    System.out.println("Introduce un valor horario:");
-    System.out.print("Horas: ");
-    int hours = in.nextInt();
-    System.out.print("Minutos: ");
-    int minutes = in.nextInt();
-    System.out.print("Segundos: ");
-    int seconds = in.nextInt();
-    return new Time(hours, minutes, seconds);
+  @Test
+  void testSetHours() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  void testSetMinutes() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  void testSetSeconds() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  void testAddSeconds() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  void testAddMinutes() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  void testAddHours() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  void testToString() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  void testCompareTo() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  void testEqualsObject() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  void testClone() {
+    fail("Not yet implemented");
   }
 
 }
