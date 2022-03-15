c8fcbc2f97d6d15879b251b9da46f97639bc4eee
Rafael del Castillo Gomariz <rafa.castillo@gmail.com>
2022-03-15 12:23:37 +0100

    Clase con utilidades varias

---
+++ org/iesgrancapitan/PROGR/ejercicios/util/Util.java
@@ -0,0 +1,44 @@
+package org.iesgrancapitan.PROGR.ejercicios.util;
+
+import java.util.Scanner;
+
+/**
+ * Conjunto de funciones genéricas que me serán de utilidad para simplificar procesos.
+ * 
+ * @author Rafael del Castillo Gomariz
+ *
+ */
+
+public class Util {
+  
+  private static Scanner in = new Scanner(System.in);
+
+  public static String readStr(String prompt) {
+    System.out.print(prompt + ": ");
+    return in.nextLine();
+  }
+  
+  public static String readStr(String prompt, String end) {
+    String str = readStr(prompt);
+    System.out.print(end);
+    return str;
+  }
+
+  public static int readInt(String prompt) {
+    System.out.print(prompt + ": ");
+    int n = in.nextInt();
+    in.nextLine();  // sacamos \n del buffer de teclado
+    return n;
+  }
+  
+  public static int readInt(String prompt, String end) {
+    int n = readInt(prompt);
+    System.out.print(end);
+    return n;
+  }
+  
+  public static int randomInt(int min, int max) {
+    return min + (int) (Math.random() * (max-min+1));
+  }
+
+}
